595eedc1c47eccf2ac38032bebf8c13fb11dcc15
Brett Chiu <chiubrett2500@gmail.com>
2024-11-25 12:07:13 -0800

    Updated repo

--- pygame_frontend.py
+++ pygame_frontend.py
@@ -6,12 +6,12 @@ pygame.mouse.set_visible(True)
 clock = pygame.time.Clock()
 running = True
 
-wood_bg = pygame.image.load("wood_bg.jpg")
+wood_bg = pygame.image.load("wood_bg.jpeg")
 
 while running:
 
     ### Create a card interface
     ### SET wood_bg as background
-    ### 
+    ###
     clock.tick(60)
 pygame.quit()
